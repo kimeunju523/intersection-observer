9fde6ad770e53c4da12c31940d881e5cb1923c69
kimeunju <lov.ej.eg.ms@gmail.com>
2023-11-22 11:29:56 +0900

    first commit

---
+++ src/components/DetectArea.jsx
@@ -0,0 +1,12 @@
+const DetectArea = ({ detectAreaRef }) => {
+  return (
+    <div
+      ref={detectAreaRef}
+      className="bg-white text-white w-full py-2 flex justify-center items-center text-xs"
+    >
+      DetectArea
+    </div>
+  );
+};
+
+export default DetectArea;
